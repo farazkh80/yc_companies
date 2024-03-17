7e0eb7c8bea551cf17abb2fd6a5cd7eedcf83b39
Faraz Khoubsirat <farazkh80@AlphaMacbook.localdomain>
2024-03-17 14:22:28 -0400

    added titles and summaries

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import os
 
 from embed import generate_embeddings, load_embeddings
 from cluster import perform_clustering, load_cluster_labels, reduce_embeddings
-from summarize import generate_summaries
+from summarize import get_titles_and_summaries
 
 EMBEDDINGS_PATH = 'data/embeddings.npy'
 REDUCED_EMBEDDINGS_PATH = 'data/reduced_embeddings.npy'
@@ -22,6 +22,7 @@ with open('yc_companies.json', 'r') as file:
 texts = [item['description'] for item in data]
 company_names = [item['name'] for item in data]
 descriptions = [item['description'] for item in data]
+links = [item['link'] for item in data]
 
 # Check for and generate/load embeddings
 if not os.path.exists(EMBEDDINGS_PATH):
@@ -42,25 +43,32 @@ plot_data = pd.DataFrame({
     'y': reduced_embeddings[:, 1],
     'label': labels,
     'company': company_names,
-    'description': descriptions
+    'description': descriptions,
+    'link': links
 })
 
+# Generate titles and summaries
+titles, summaries = get_titles_and_summaries(labels, company_names, descriptions)
+
+# Add titles to the plot data
+plot_data['title'] = plot_data['label'].map(titles)
+
 # Streamlit visualization with Altair
-st.title("Clustered Company Data Visualization")
+st.title("Clustered Visualization of Y Combinator Companies")
+st.write(f"**Total Companies analyzed**:\t{len(data)}")
 
 chart = alt.Chart(plot_data).mark_circle(size=60).encode(
     x='x',
     y='y',
-    color='label:N',
-    tooltip=['company', 'description', 'label']
+    color='title',
+    tooltip=['company', 'description', 'title', 'link']
 ).interactive()
 
 st.altair_chart(chart, use_container_width=True)
 
-# Add a UI element to let the user choose the summarization API
-use_openai = st.checkbox("Use OpenAI for summarization", value=True)
-
-# Then, when generating summaries:
-summaries = generate_summaries(labels, company_names, descriptions, use_openai=use_openai)
-
-st.write(summaries)
+# Display summaries
+for label, summary in summaries.items():
+    st.write(f"## Cluster {titles[label]}")
+    # number of companies in the cluster
+    st.write(f"**Number of companies in this cluster**: {len(plot_data[plot_data['label'] == label])}")
+    st.write(summary)
--- summarize.py
+++ summarize.py
@@ -1,89 +1,120 @@
 import os
 import collections
+from tqdm import tqdm
+import json
+
 from openai import OpenAI
 import cohere
-from tqdm import tqdm
 
+from cluster import K
+
+# Constants
 API_KEY = os.environ['COHERE_API_KEY']
-OPENAI_API_KEY = os.environ['OPENAI_API_KEY']
-SUMMARY_FILE = 'data/descriptions.txt'
-K = 8
+COMMAND = 'command'
+CHAT_COMMAND = 'command-r'
 
-def generate_summaries_openai(prompt):
-    client = OpenAI()
-    # use gpt-3.5-turbo for faster response times
-    response = client.chat.completions.create(
-        model = "gpt-3.5-turbo",
-        messages = [
-            {
-                "role": "system",
-                "content": "You are given a group of companies with similar descriptions of what they do. Analyze these companies' descriptions, find trends and insights from them as a group. Report any patterns you find from these companies' field of focus, target customers, and why they relate to each other. Lastly, give at least 5 examples of companies in this cluster that support your trend and pattern findings. Here are the companies in this cluster for you to analyze:"
-            },
-            {
-                "role": "user",
-                "content": prompt
-            }
-        ],
-        max_tokens=512
-    )
 
-    print(response.choices[0].message.content)
-    return response.choices[0].message.content
-        
+# Define the paths to the files
+TITLES_FILE = 'data/titles.json'
+SUMMARIES_FILE = 'data/summaries.json'
 
-def generate_summaries_cohere(prompt):
+
+TITLE_FORMAT = "word_1, word_2, word_3"
+SUMMARY_FORMAT = f"""
+**Area of focus**: a paragraph describing the area of focus, target audience, target market, and commonly used technologies of the companies in this cluster.
+
+**Trends and Patterns**:
+- **Trend 1**: Brief description
+- **Trend 2**: Brief description
+- **Trend 3**: Brief description
+
+**Examples**:
+- Company 1: one line summary from the given context
+- Company 2: one line summary from the given context
+- Company 3: one line summary from the given context
+- Company 4: one line summary from the given context
+- Company 5: one line summary from the given context
+
+"""
+
+
+
+
+def chat_cluster_title_cohere(combined_text):
+    """Generate a 3-word title for a cluster using Cohere's 'chat' model"""
     co = cohere.Client(API_KEY)
-    response = co.generate(model='command_nightly', prompt=prompt, stop_sequences=[], max_tokens=200)
-    return response.generations[0].text
+    chat_history = [
+        {"role": "system", "message": "You are given a group of companies and their one line summaries, and supposed to generate a 3-word title that represents the companies and their descriptions. The output should be a 3-word title with no extra tokens."},
+    ]
+    message =f"""Generate a 3-word title that represents the following companies and their descriptions down below.  The title should be 3 words and entail things like area of focus, target audience and market, and used technologies.\n
+    Your output should follow format of
+    {TITLE_FORMAT}
 
-def generate_summaries(labels, company_names, descriptions, use_openai=True):
-    sdk = 'cohere' if not use_openai else 'openai'
-    summary_file = SUMMARY_FILE.replace('.txt', f'_{sdk}.txt')
-    if not os.path.exists(summary_file):
+    # Company Descriptions
+    {combined_text}
+    """
+    response = co.chat(model=CHAT_COMMAND, message=message, max_tokens=10, chat_history=chat_history)
+    return response.text
+
+def chat_summaries_cohere(combined_text):
+    """Generate summaries using Cohere's 'chat' model"""
+    co = cohere.Client(API_KEY)
+    chat_history = [
+        {"role": "system", "message": "You are given a group of companies and their one line summaries, and supposed to generate a summary that represents the companies and their descriptions."}
+    ]
+    message = f"""Generate a summary that represents the following companies and their description provided down below, your output should follow the fornat
+    {SUMMARY_FORMAT}
+
+    # Company Descriptions   
+    {combined_text}
+    """
+    response = co.chat(model=CHAT_COMMAND, message=message, max_tokens=400, chat_history=chat_history)
+    return response.text
+
+def get_titles_and_summaries(labels, company_names, descriptions):
+    """Main function to generate summaries"""
+    # If both files exist, read the titles and summaries from the files
+    if os.path.exists(TITLES_FILE) and os.path.exists(SUMMARIES_FILE):
+        with open(TITLES_FILE, 'r') as f:
+            titles = json.load(f)
+            titles = {int(k): v for k, v in titles.items()}
+        with open(SUMMARIES_FILE, 'r') as f:
+            summaries = json.load(f)
+            summaries = {int(k): v for k, v in summaries.items()}
+    else:
+        # Group companies by cluster labels
         clustered_companies = collections.defaultdict(list)
         for i, label in enumerate(labels):
-            cluster_entry = {
-                'name': company_names[i],
-                'description': descriptions[i]
-            }
+            cluster_entry = {'name': company_names[i], 'description': descriptions[i]}
             clustered_companies[label].append(cluster_entry)
 
-        with open(summary_file, 'w') as f:
-            for label in tqdm(range(K), desc='Summarizing clusters'):
-                companies = clustered_companies[label]
-                combined_text = ' '.join([f"{company['name']}: {company['description']}" for company in companies])
-                prompt = f"""
-                You are given a group of companies with similar descriptions of what they do. Analyze these companies' descriptions, find trends and insights from them as a group.
-                Report any patterns you find from these companies' field of focus, target customers, and why they relate to each other.
-                Lastly, give at least 5 examples of companies in this cluster that support your trend and pattern findings.
-
-                Your answer should be in following format:
-
-                \n**Area of focus**: 1 paragraph explaining the area of focus of the companies in this cluster and their target customers
-                \n**Trends and Patterns**:
-                    - 
-                    -
-                    -
-                \n**Examples**:
-                    - company name: company description
-                    - company name: company description
-                    - company name: company description
-                    - company name: company description
-                    - company name: company description
-
-                Here are the companies in this cluster for you to analyze:
-                {combined_text}"""
-
-                if use_openai:
-                    summary = generate_summaries_openai(prompt)
-                else:
-                    summary = generate_summaries_cohere(prompt)
-                
-                f.write(f"## Cluster {label} Summary\n")
-                f.write(summary)
-                f.write("\n\n")
+        # Store the titles in a dictionary
+        titles = {}
+        # Summaries in a dictionary
+        summaries = {}
         
-    with open(summary_file, 'r') as f:
-            summaries = f.read()
-    
-    return summaries
+        for label in tqdm(range(K), desc='Summarizing clusters'):
+            # Combine company names and descriptions
+            companies = clustered_companies[label]
+            combined_text = ' '.join([f"{company['name']}: {company['description']}" for company in companies])
+
+            # Generate the cluster title
+            title = chat_cluster_title_cohere(combined_text)
+
+            # Store the title in the dictionary
+            titles[label] = title
+
+            # Generate the summary
+            summary = chat_summaries_cohere(combined_text)
+
+
+            # Store the summary in the dictionary
+            summaries[label] = summary
+            
+        # Write the titles and summaries to files
+        with open(TITLES_FILE, 'w') as f:
+            json.dump(titles, f)
+        with open(SUMMARIES_FILE, 'w') as f:
+            json.dump(summaries, f)
+
+    return titles, summaries
